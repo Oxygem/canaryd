58d6ba2e6f96836fb7924683d33ab532bd974d26
Nick Barrett <nick@oxygem.com>
2017-03-02 22:42:41 +0000

    Add tiny readme, +release script.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # canaryd
+
+[Service Canary](https://servicecanary.com) daemon that collects and uploads system state.
---
+++ scripts/release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+VERSION=`python setup.py --version`
+
+echo "# Releasing canaryd v$VERSION..."
+
+echo "# Running tests..."
+nosetests -s
+
+echo "# Git tag & push..."
+git tag -a "v$VERSION" -m "v$VERSION"
+git push --tags
+
+echo "# Upload to pypi..."
+# Clear dist
+rm -rf dist/*
+# Build source and wheel packages
+python setup.py sdist bdist_wheel
+# Upload w/Twine
+twine upload dist/*
+
+echo "# All done!"
