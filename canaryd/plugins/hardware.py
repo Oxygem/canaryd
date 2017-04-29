61ec376d332c14ab92a36220359c41e8546192ad
Nick Barrett <nick@oxygem.com>
2017-04-29 19:19:23 +0200

    Rewrite `hardware` plugin to use `lshw`, not `dmidecode`, for better coverage.

--- canaryd/plugins/hardware.py
+++ canaryd/plugins/hardware.py
@@ -1,45 +1,78 @@
+import json
+
 from canaryd.packages import six
-from canaryd.packages.dmidecode import parse_dmi
 
 from canaryd.plugin import Plugin
+
+LSHW_CLASSES = ('bridge', 'processor', 'memory', 'disk', 'network')
+TOP_LEVEL_DICT_KEYS = ('capabilities', 'configuration')
+TOP_LEVEL_STRING_KEYS = (
+    'serial', 'version', 'vendor',
+    'product', 'description', 'handle',
+)
 
 
 def slugify(value):
     return value.lower().replace(' ', '_')
 
 
-class Hardware(Plugin):
-    spec = ('key', {
-        'type': six.text_type,
-        'serial_number': six.text_type,
-        'version': six.text_type,
-        'meta': dict,
-    })
+def get_lshw_items(children):
+    items = []
 
-    command = 'dmidecode'
+    for item in children:
+        if 'children' in item:
+            items.extend(get_lshw_items(item.pop('children')))
+
+        if item['class'] in LSHW_CLASSES:
+            new_item = {
+                'type': item.pop('class'),
+            }
+
+            for k in TOP_LEVEL_STRING_KEYS + TOP_LEVEL_DICT_KEYS:
+                if k in item:
+                    new_item[k] = item.pop(k)
+
+            new_item['meta'] = item
+
+            # Prefer logicalname over id
+            key = item.pop('logicalname', item.pop('id'))
+
+            items.append((key, new_item))
+
+    return items
+
+
+def get_hardware_spec():
+    hardware_spec = {
+        'type': six.text_type,
+        'meta': dict,
+    }
+
+    for key in TOP_LEVEL_STRING_KEYS:
+        hardware_spec[key] = six.text_type
+
+    for key in TOP_LEVEL_DICT_KEYS:
+        hardware_spec[key] = dict
+
+    return hardware_spec
+
+
+class Hardware(Plugin):
+    spec = ('key', get_hardware_spec())
+
+    command = 'lshw -json'
 
     @staticmethod
     def parse(output):
-        dmi_data = parse_dmi(output)
-
-        data = []
-
-        for key, meta in dmi_data:
-            # Make _title -> title
-            meta['title'] = meta.pop('_title')
-
-            meta = dict(
-                (slugify(k), value)
-                for k, value in six.iteritems(meta)
-            )
-
-            item = {}
-            for k in ('type', 'serial_number', 'version'):
-                if k in meta:
-                    item[k] = meta.pop(k)
-
-            item['meta'] = meta
-
-            data.append((slugify(key), item))
+        lshw_data = json.loads(output)
+        data = get_lshw_items(lshw_data.get('children', []))
 
         return dict(data)
+
+    @staticmethod
+    def is_change(key, previous_item, item):
+        # If any of our top level keys (serial, vendor, product, etc) change,
+        # count as a change. Anything nested (meta, capabilities, etc) is ignored.
+        for key in TOP_LEVEL_STRING_KEYS:
+            if previous_item.get(key) != item.get(key):
+                return True
