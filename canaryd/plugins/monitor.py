04e40da9504107b9ad6a48ce68c5360add9d1036
Nick Barrett <nick@oxygem.com>
2017-05-01 17:11:06 +0200

    Add basic monitor plugin that tracks CPU, memory and disk percentages including 5/10/15 minute averages.

---
+++ canaryd/plugins/monitor.py
@@ -0,0 +1,226 @@
+from __future__ import division
+
+from collections import defaultdict, deque
+from itertools import islice
+from subprocess import CalledProcessError
+from time import sleep, time
+
+from canaryd.packages import six
+
+from canaryd.log import logger  # noqa
+from canaryd.plugin import Plugin
+
+from .monitor_util import get_cpu_stats, get_disk_stats, get_memory_stats
+
+
+def sleep_until_interval(start, interval):
+    time_taken = time() - start
+
+    if time_taken < interval:
+        sleep(interval - time_taken)
+
+
+class Monitor(Plugin):
+    '''
+    This plugin monitors the servers basics (CPU/memory/disk/etc) and provides
+    warning/critical alerts as configured on ``app.servicecanary.com``.
+
+    Tracks:
+
+    + CPU/IO wait usage %
+    + Memory/swap usage %
+    + Per disk usage %
+
+    The actual tracking happens in a separate thread.
+    '''
+
+    spec = ('key', {
+        'type': six.text_type,
+
+        # The value, max and the % used
+        'percentage': float,
+        'value': int,
+        'max': int,
+
+        # Rolling percentage averages
+        '1_min_percentage': float,
+        '5_min_percentage': float,
+        '15_min_percentage': float,
+    })
+
+    collect_interval = None
+
+    def setup_history(self):
+        # Number of items needed if one per collect interval to total 15 mins
+        intervals = (15 * 60) / self.collect_interval
+        logger.info('Setup interval history with {0} slots'.format(intervals))
+
+        self.history = defaultdict(lambda: deque((), intervals))
+
+    def prepare(self, settings):
+        self.collect_interval = settings.collect_interval_s
+        self.setup_history()
+
+    def get_stats(self):
+        seen_keys = set()
+
+        for type_, collector in (
+            ('cpu', get_cpu_stats),
+            ('memory', get_memory_stats),
+            ('disk', get_disk_stats),
+        ):
+            try:
+                stats = collector()
+
+            except (CalledProcessError, OSError) as e:
+                logger.warning('Error collecting {0} stats: {1}'.format(type_, e))
+                continue
+
+            # Push all the stats -> history
+            for key, data in six.iteritems(stats):
+                data['type'] = type_
+                data['percentage'] = round(data['percentage'], 2)
+                self.history[key].appendleft(data)
+                seen_keys.add(key)
+
+        # Remove anything not found in the latest stats
+        history_keys = set(six.iterkeys(self.history))
+        for key_to_remove in history_keys - seen_keys:
+            self.history.pop(key_to_remove)
+
+    def get_state(self, settings):
+        # If the collect interval has changed, reset history otherwise we'll
+        # generate bad averages.
+        if self.collect_interval != settings.collect_interval_s:
+            self.collect_interval = settings.collect_interval_s
+            self.setup_history()
+
+        # Get all the latest stats
+        self.get_stats()
+
+        intervals = {
+            '1_min_percentage': max(60 / self.collect_interval, 1),
+            '5_min_percentage': max((5 * 60) / self.collect_interval, 1),
+            '15_min_percentage': max((15 * 60) / self.collect_interval, 1),
+        }
+
+        data = {}
+
+        for key, history in six.iteritems(self.history):
+            if not history:
+                continue
+
+            data[key] = history[0]
+
+            for interval_key, interval_length in six.iteritems(intervals):
+                if len(history) >= interval_length:
+                    data[key][interval_key] = round(sum(
+                        item['percentage']
+                        for item in list(islice(history, 0, interval_length))
+                    ) / interval_length, 2)
+
+        return data
+
+    @staticmethod
+    def event_message(type_, key, data_changes):
+        if type_ not in ('added', 'deleted'):
+            return
+
+        if 'type' not in data_changes:
+            return
+
+        type_name, new_type_name = data_changes['type']
+
+        if type_ == 'added':
+            type_name = new_type_name
+
+        return '{0} {1}: {2}'.format(type_name.title(), type_, key)
+
+    @staticmethod
+    def generate_events(type_, key, data_changes, settings):
+        settings_key = key
+
+        if key not in ('cpu', 'memory'):
+            settings_key = 'disk'
+
+        def make_event(type_, limit, time, changes):
+            message_key = key
+
+            if settings_key != 'disk':
+                message_key = message_key.title()
+
+            message = '{0} is over {1}%'.format(message_key, limit)
+
+            if time > 0:
+                if time == 60:
+                    time = '1 minute'
+
+                elif time == 300:
+                    time = '5 minutes'
+
+                elif time == 900:
+                    time = '15 minutes'
+
+                message = '{0} has been over {1}% for {2}'.format(
+                    message_key, limit, time,
+                )
+
+            if limit is None:
+                message = '{0} is back to normal'.format(message_key)
+
+            return type_, message, changes
+
+        # If the item has been removed, resolve any lefover issues and exit
+        if type_ == 'deleted':
+            yield make_event('resolved', None, None, None)
+            return
+
+        time_setting_to_key = {
+            0: 'percentage',
+            60: '1_min_percentage',
+            300: '5_min_percentage',
+            900: '15_min_percentage',
+        }
+
+        # Status of over-limit values (is_warning, was_warning)
+        resolved_changes = {}
+        wanted_data_keys = set()
+
+        for type_ in ('critical', 'warning'):
+            enabled = getattr(
+                settings,
+                '{0}_{1}'.format(settings_key, type_),
+            )
+
+            if not enabled:
+                continue
+
+            limit = getattr(
+                settings,
+                # Eg cpu_warning_limit
+                '{0}_{1}_limit'.format(settings_key, type_),
+            )
+
+            time = getattr(
+                settings,
+                '{0}_{1}_time_s'.format(settings_key, type_),
+            )
+
+            if time in time_setting_to_key:
+                data_key = time_setting_to_key[time]
+                wanted_data_keys.add(data_key)
+
+                if data_key in data_changes:
+                    old_value, value = data_changes[data_key]
+
+                    if value > limit:
+                        yield make_event(type_, limit, time, {
+                            data_key: data_changes[data_key],
+                        })
+                        # This works because critical is run before warning
+                        return
+
+                    resolved_changes[data_key] = data_changes[data_key]
+
+        if all(key in resolved_changes for key in wanted_data_keys):
+            yield make_event('resolved', None, None, resolved_changes)
---
+++ canaryd/plugins/monitor_util.py
@@ -0,0 +1,246 @@
+from __future__ import division
+
+import re
+import sys
+
+from multiprocessing import cpu_count
+from os import path
+
+from canaryd.packages import six
+from canaryd.packages.check_output import check_output
+
+DF_REGEX = r'([a-zA-Z0-9\/\-_]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]{1,3})%\s+([a-zA-Z\/0-9\-_\s]+)'
+
+
+def get_ps_cpu_stats():
+    '''
+    Uses ps + awk to total CPU usage, then divide by # CPUs to get the final %.
+    '''
+
+    output = check_output(
+        "ps -A -o %cpu | awk '{s+=$1} END {print s}'",
+        shell=True,
+    )
+
+    try:
+        cpu_percentage = float(output.strip())
+
+    except (TypeError, ValueError):
+        return
+
+    return {
+        'cpu': {
+            'percentage': cpu_percentage / cpu_count(),
+        },
+    }
+
+
+def get_proc_cpu_stats():
+    '''
+    Parses 2x /proc/stat output to calculate CPU %.
+    '''
+
+    output = check_output(
+        'cat /proc/stat && sleep 1 && cat /proc/stat',
+        shell=True,
+    )
+
+    # Parse /proc/stat
+    columns = ['user', 'nice', 'system', 'idle', 'iowait', 'irq', 'soft_irq']
+    cpus = {}
+    diffs = None
+    total = None
+
+    for line in output.splitlines():
+        bits = line.split()
+        key, details = bits[0], [int(bit) for bit in bits[1:8]]
+
+        if key == 'cpu':
+            if key in cpus:
+                # Calculate usage (second cat)
+                diffs = dict(
+                    (column, details[i] - cpus[key][i])
+                    for i, column in enumerate(columns)
+                )
+                total = sum(six.itervalues(diffs))
+
+            else:
+                # Set details (first cat)
+                cpus[key] = details
+
+    return {
+        'cpu': {
+            'value': total - diffs['idle'],
+            'max': total,
+            'percentage': (total - diffs['idle']) / total * 100,
+        },
+        'iowait': {
+            'value': diffs['iowait'],
+            'max': total,
+            'percentage': diffs['iowait'] / total * 100
+        },
+    }
+
+
+def get_cpu_stats():
+    # If `ps` command used instead of preferred /proc/stat, quickly work
+    # out the CPU % and use that (no iowait support).
+    if not path.exists('/proc'):
+        return get_ps_cpu_stats()
+
+    # Parse /proc/stat
+    return get_proc_cpu_stats()
+
+
+def parse_memory_stats(
+    lines,
+    value_is_last=False,
+    value_multiplier=1,
+    value_divisor=1,
+):
+    key_map = {
+        'MemTotal:': 'memory_total',
+        'MemFree:': 'memory_free',
+        'Buffers:': 'memory_free',
+        'Cached:': 'memory_free',
+
+        'SwapTotal:': 'swap_total',
+        'SwapFree:': 'swap_free',
+        'SwapCached:': 'swap_free',
+
+        'Pages free:': 'memory_free',
+        'Pages inactive:': 'memory_free',
+    }
+
+    stats = {}
+
+    for line in lines:
+        bits = line.split()
+
+        if value_is_last:
+            key = ' '.join(bits[:-1])
+            value = bits[-1][:-1]
+        else:
+            key, value = bits[:2]
+
+        if key in key_map:
+            value = int(value) * value_multiplier / value_divisor
+
+            if key_map[key] in stats:
+                stats[key_map[key]] += value
+                continue
+
+            stats[key_map[key]] = value
+
+    return stats
+
+
+def get_proc_memory_stats():
+    '''
+    Parses /proc/meminfo output.
+    '''
+
+    output = check_output(
+        'cat /proc/meminfo',
+        shell=True,
+    )
+
+    stats = parse_memory_stats(
+        output.splitlines(),
+        value_divisor=1024,
+    )
+
+    data = {
+        'memory': {
+            'value': int(stats['memory_total'] - stats['memory_free']),
+            'max': int(stats['memory_total']),
+            'percentage': round(
+                (stats['memory_total'] - stats['memory_free']) / stats['memory_total'] * 100,
+                2,
+            ),
+        },
+    }
+
+    if stats['swap_total'] > 0:
+        data['swap'] = {
+            'value': int(stats['swap_total'] - stats['swap_free']),
+            'max': int(stats['swap_total']),
+            'percentage': round(
+                (stats['swap_total'] - stats['swap_free']) / stats['swap_total'] * 100,
+                2,
+            ),
+        }
+
+    return data
+
+
+def get_vm_stat_memory_stats():
+    output = check_output(
+        'sysctl hw.memsize',
+        shell=True,
+    )
+
+    bits = output.split()
+    total_memory = int(bits[1]) / 1024 / 1024
+
+    output = check_output(
+        'vm_stat',
+        shell=True,
+    )
+
+    stats = parse_memory_stats(
+        output.splitlines(),
+        value_is_last=True,
+        value_multiplier=4096,
+        value_divisor=1024 * 1024,
+    )
+
+    return {
+        'memory': {
+            'value': int(total_memory - stats['memory_free']),
+            'max': int(total_memory),
+            'percentage': round(
+                (total_memory - stats['memory_free']) / total_memory * 100,
+                2,
+            ),
+        },
+    }
+
+
+def get_memory_stats():
+    # If `ps` command used instead of preferred /proc/stat, quickly work
+    # out the CPU % and use that (no iowait support).
+    if sys.platform == 'darwin':
+        return get_vm_stat_memory_stats()
+
+    # Parse /proc/stat
+    return get_proc_memory_stats()
+
+
+def get_disk_stats():
+    '''
+    Parses df output.
+    '''
+
+    output = check_output('df -k', shell=True)
+
+    devices = {}
+
+    for line in output.splitlines():
+        matches = re.match(DF_REGEX, line)
+        if matches:
+            if matches.group(1) in ('none', 'udev', 'tmpfs'):
+                continue
+
+            value = int(matches.group(3))
+            max_ = int(matches.group(2))
+
+            percentage = round(value / max_ * 100, 2)
+
+            devices[matches.group(6)] = {
+                'percentage': percentage,
+                'value': int(round(value / 1024)),
+                'max': int(round(max_ / 1024)),
+            }
+
+    return devices
