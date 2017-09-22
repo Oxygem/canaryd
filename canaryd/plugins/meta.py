61fd49f0d70a1898d8979f341326b2d70faa511e
Nick Barrett <nick@oxygem.com>
2017-09-22 17:37:06 +0100

    Add arch and kernel name/release to meta plugin.

--- canaryd/plugins/meta.py
+++ canaryd/plugins/meta.py
@@ -3,6 +3,8 @@ import socket
 
 from datetime import datetime, timedelta
 
+from canaryd.packages.check_output import check_output
+
 from canaryd.plugin import Plugin
 
 UPTIME_REGEX = re.compile((
@@ -19,54 +21,74 @@ def ensure_datetime(datetime_or_string):
     return datetime.strptime(datetime_or_string, '%Y-%m-%dT%H:%M:%S')
 
 
+def get_uptime_and_users():
+    data = []
+    output = check_output('uptime', shell=True)
+
+    for line in output.splitlines():
+        line = line.strip()
+        matches = re.search(UPTIME_REGEX, line)
+
+        if matches:
+            duration, users, av1, av5, av15 = matches.groups()
+            data.append(('users', int(users)))
+
+            days = 0
+            hours = 0
+            mins = 0
+
+            if 'day' in duration:
+                match = re.search('([0-9]+)\s+day', duration)
+                days = int(match.group(1))
+
+            if ':' in duration:
+                match = re.search('([0-9]+):([0-9]+)', duration)
+                hours = int(match.group(1))
+                mins = int(match.group(2))
+
+            if 'min' in duration:
+                match = re.search('([0-9]+)\s+min', duration)
+                mins = int(match.group(1))
+
+            up_since = datetime.utcnow() - timedelta(
+                days=days, hours=hours, minutes=mins,
+            )
+
+            up_since = up_since.replace(
+                second=0,
+                microsecond=0,
+            )
+
+            data.append(('up_since', up_since))
+
+    return data
+
+
+def get_uname_data(flag):
+    output = check_output(
+        'uname -{0}'.format(flag),
+        shell=True,
+    )
+
+    return output.strip()
+
+
 class Meta(Plugin):
     spec = ('key', {
         'value': None,
     })
 
-    command = 'uptime'
+    @staticmethod
+    def prepare(settings):
+        pass
 
     @staticmethod
-    def parse(output):
-        data = [
-            ('hostname', socket.gethostname()),
-        ]
-
-        for line in output.splitlines():
-            line = line.strip()
-            matches = re.search(UPTIME_REGEX, line)
-
-            if matches:
-                duration, users, av1, av5, av15 = matches.groups()
-                data.append(('users', int(users)))
-
-                days = 0
-                hours = 0
-                mins = 0
-
-                if 'day' in duration:
-                    match = re.search('([0-9]+)\s+day', duration)
-                    days = int(match.group(1))
-
-                if ':' in duration:
-                    match = re.search('([0-9]+):([0-9]+)', duration)
-                    hours = int(match.group(1))
-                    mins = int(match.group(2))
-
-                if 'min' in duration:
-                    match = re.search('([0-9]+)\s+min', duration)
-                    mins = int(match.group(1))
-
-                up_since = datetime.utcnow() - timedelta(
-                    days=days, hours=hours, minutes=mins,
-                )
-
-                up_since = up_since.replace(
-                    second=0,
-                    microsecond=0,
-                )
-
-                data.append(('up_since', up_since))
+    def get_state(settings):
+        data = get_uptime_and_users()
+        data.append(('hostname', socket.gethostname()))
+        data.append(('kernel', get_uname_data('s')))
+        data.append(('kernel_release', get_uname_data('r')))
+        data.append(('arch', get_uname_data('m')))
 
         # Nest each value in a dict
         return dict(
@@ -78,9 +100,7 @@ class Meta(Plugin):
 
     @staticmethod
     def is_change(key, previous_item, item):
-        if key == 'hostname':
-            return True
-
+        # Check to see if we rebooted, if not there's no change
         if key == 'up_since':
             previous_up = ensure_datetime(previous_item['value'])
             up = ensure_datetime(item['value'])
@@ -92,8 +112,14 @@ class Meta(Plugin):
                 and (previous_up.minute + 1) < up.minute
             ):
                 return True
+            else:
+                return False
 
-        return False
+        # The # of active users shouldn't generate events!
+        if key == 'users':
+            return False
+
+        return True
 
     @staticmethod
     def event_message(type_, key, data_changes):
