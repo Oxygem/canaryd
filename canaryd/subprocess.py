358df6514b19424d5576d7a0a74ea15d71a3565b
Nick Barrett <nick@oxygem.com>
2018-06-20 12:52:31 +0100

    Fix for python2.6: decode doesn't take keyword arguments.

--- canaryd/subprocess.py
+++ canaryd/subprocess.py
@@ -33,6 +33,6 @@ def get_command_output(command, *args, **kwargs):
 
     if isinstance(output, six.binary_type):
         encoding = chardet.detect(output)['encoding']
-        output = output.decode(encoding=encoding)
+        output = output.decode(encoding)
 
     return output
