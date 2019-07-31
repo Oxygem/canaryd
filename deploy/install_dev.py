eb47c76e4b0eae57412a5dec4e4531fdcddbbfb7
Nick Barrett <nick@oxygem.com>
2019-07-31 20:05:26 +0100

    Fix pyinfra dev install for CentOS 6.

--- deploy/install_dev.py
+++ deploy/install_dev.py
@@ -1,5 +1,5 @@
 from pyinfra import host
-from pyinfra.modules import files, pkg, server
+from pyinfra.modules import files, pkg, server, yum
 
 SUDO = True
 
@@ -18,13 +18,24 @@ if host.name == '@vagrant/openbsd58':
     )
 
 
-server.shell(
-    {'Install pip'},
-    (
-        'wget https://bootstrap.pypa.io/get-pip.py',
-        'python get-pip.py',
-    ),
-)
+# CentOS 6 / python2.6 can't use get-pip.py
+if host.name == '@vagrant/centos6':
+    yum.rpm(
+        {'Install the epel repos'},
+        'https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm',
+    )
+    yum.packages(
+        {'Install python-pip'},
+        ['python-pip'],
+    )
+else:
+    server.shell(
+        {'Install pip'},
+        (
+            'wget https://bootstrap.pypa.io/get-pip.py',
+            'python get-pip.py',
+        ),
+    )
 
 
 server.shell(
