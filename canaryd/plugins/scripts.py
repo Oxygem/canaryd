00d672b66edbff4926e573e39c2d178e3587a539
Nick Barrett <nick@oxygem.com>
2019-08-19 18:58:13 +0100

    Add enabled key to scripts spec and include all scriots, enabled or not.

--- canaryd/plugins/scripts.py
+++ canaryd/plugins/scripts.py
@@ -36,24 +36,27 @@ class Scripts(Plugin):
     spec = ('script', {
         'output': six.text_type,
         'exit_code': int,
+        'enabled': bool,
     })
 
-    # Don't generate events from state updates (see generate_issues_from_key_change below)
+    # Don't generate events from state updates (see generate_issues_from_change below)
     generate_update_events = False
 
     def prepare(self, settings):
         pass
 
     def get_state(self, settings):
-        scripts = [
-            (script, path.join(get_scripts_directory(), 'enabled', script))
-            for script, enabled in get_scripts(settings)
-            if enabled
-        ]
-
         results = {}
 
-        for script, script_path in scripts:
+        for script, enabled in get_scripts(settings):
+            if not enabled:
+                results[script] = {
+                    'enabled': False,
+                }
+                continue
+
+            script_path = path.join(get_scripts_directory(), 'enabled', script)
+
             try:
                 output = get_command_output(
                     shlex_quote(script_path),
@@ -79,6 +82,16 @@ class Scripts(Plugin):
         # If the script has been removed, resolve any leftover issues and exit.
         # (the delete event is still created).
         if change.type == 'deleted':
+            yield 'resolved', None, None
+            return
+
+        # If the script has been disabled on the host, resolve any leftover issues
+        # and exit.
+        if (
+            change.type == 'updated'
+            and 'enabled' in data_changes
+            and data_changes['enabled'][1] is False
+        ):
             yield 'resolved', None, None
             return
 
